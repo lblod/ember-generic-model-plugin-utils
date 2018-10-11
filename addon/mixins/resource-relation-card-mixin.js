623f4907c663364e1e356f6434a451fe0126d901
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-10-11 12:39:31 +0200

    first version of library

---
+++ addon/mixins/resource-relation-card-mixin.js
@@ -0,0 +1,30 @@
+import Mixin from '@ember/object/mixin';
+import { inject as service } from '@ember/service';
+import CardMixin from './card-mixin';
+import {
+  formatClassDisplay,
+  parseJSONAPIResults,
+  extendedRdfa,
+  relationPropertyToRdfaReference
+} from '../utils/json-api-to-rdfa';
+
+export default Mixin.create(CardMixin, {
+  ajax: service(),
+  async refer(relationMeta, JSONAPIResource, displayLabel, hintOwner){
+    let mappedLocation = this.get('hintsRegistry').updateLocationToCurrentIndex(this.get('hrId'), this.get('location'));
+    this.get('hintsRegistry').removeHintsAtLocation(this.get('location'), this.get('hrId'), hintOwner);
+    this.get('editor').replaceTextWithHTML(...mappedLocation,
+                                           relationPropertyToRdfaReference(relationMeta,
+                                                                           await relationMeta.get('range'),
+                                                                           JSONAPIResource,
+                                                                           displayLabel));
+  },
+
+  async extend(relationMeta, JSONAPIResource, hintOwner){
+    let classMetaData = await this.rdfsClassForJsonApiType(JSONAPIResource.data.type);
+    let rdfa = await extendedRdfa(query => { return this.ajax.request(query); }, JSONAPIResource, classMetaData, relationMeta.rdfaType);
+    let mappedLocation = this.get('hintsRegistry').updateLocationToCurrentIndex(this.get('hrId'), this.get('location'));
+    this.get('hintsRegistry').removeHintsAtLocation(this.get('location'), this.get('hrId'), 'editor-plugins/generic-model-plugin');
+    this.get('editor').replaceTextWithHTML(...mappedLocation, rdfa);
+  }
+});
---
+++ app/utils/meta-model-utils.js
@@ -0,0 +1,1 @@
+export { default } from '@lblod/ember-generic-model-plugin-utils/utils/meta-model-utils';
